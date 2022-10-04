c7cae55558993cac2b4f50a669006b5c6abc92ea
vicTorKd <81010941+vicTorKd@users.noreply.github.com>
2022-10-04 21:18:13 +0800

    Update csv_process_attack.py

--- pcap_process/csv_process_attack.py
+++ pcap_process/csv_process_attack.py
@@ -3,6 +3,7 @@ import pandas as pd
 from datetime import datetime
 import os
 
+
 PACKET_NUMBER = 5
 data_set = "iot_attack"
 
@@ -19,11 +20,9 @@ def new_extract(inputName, saveName):
     # time srcIP srcPort dstIP dstPort protocol length
     col_names = ["time", "srcIP", "srcPort", "dstIP", "dstPort", "protocol", 
     "ip_ihl", "ip_tos", "ip_flags", "ip_ttl", "tcp_dataofs", "tcp_flag", "tcp_window", 
-    "udp_len", "length"]
+    "udp_len", "length", "srcMAC", "dstMAC"]
     # df = pd.read_csv(inputName, delimiter="|", names=col_names, nrows=100)
     tcp = pd.read_csv(inputName, delimiter="|", names=col_names)
-    # print(df)
-    # print(df)
 
     #tcp = df.drop(["time"], axis=1)
 
@@ -41,31 +40,33 @@ def new_extract(inputName, saveName):
 
 def open_source_data_process():
     print('main')
-    file_list = file_name_walk('../DataSets/open-source/normal-packet-level-device')
-    save_root = '../DataSets/open-source/normal-dec-feature-device'
+    file_list = file_name_walk('../DataSets/Open-Source/normal-packet-level-device')
+    save_root = '../DataSets/Open-Source/normal-dec-feature-device'
+    # file_list = file_name_walk('../DataSets/Open-Source/attack-packet-level-device')
+    # save_root = '../DataSets/Open-Source/attack-dec-feature-device'
     if not os.path.exists(save_root):
         os.makedirs(save_root)
     file_list.sort()
     for i, file_name in enumerate(file_list):
-        # try:
         print(file_name)
         if i < 10:  # for file sort, because '.' > {0-9}
             save_path = save_root + '/file-0{}.csv'.format(i)
         else:
             save_path = save_root + '/file-{}.csv'.format(i)
         new_extract(file_name, save_path)
-        # print(file_name)
-        # except:
-        #     print('fail to open ', file_name)
 
 
 def main():
-    normal_list = ['skyworth_camera','tplink_camera','xiaomi_camera']#'philips_camera','360_camera','ezviz_camera','hichip_battery_camera','mercury_wirecamera','skyworth_camera','tplink_camera','xiaomi_camera'
+    normal_list = ['philips_camera','360_camera','ezviz_camera','hichip_battery_camera','mercury_wirecamera','skyworth_camera','tplink_camera','xiaomi_camera']#'philips_camera','360_camera','ezviz_camera','hichip_battery_camera','mercury_wirecamera','skyworth_camera','tplink_camera','xiaomi_camera'
+    normal_list.extend(['aqara_gateway', 'gree_gateway', 'ihorn_gateway', 'tcl_gateway', 'xiaomi_gateway'])
+    # normal_list=os.listdir('../DataSets/Attack_iot_filter/Pcap/')
     for type_index, type_name in enumerate(normal_list):
-        #file_list = file_name_walk('../DataSets/attack/attack-packet-level-device/{}'.format(type_name))
-        #save_root = '../DataSets/attack/attack-dec-feature-device/{}'.format(type_name)
-        file_list = file_name_walk('../DataSets/our/normal-packet-level-device/{:}'.format(type_name))
-        save_root = '../DataSets/our/normal-dec-feature-device/{}'.format(type_name)
+        # file_list = file_name_walk('../DataSets/Anomaly/attack-packet-level-device/{}'.format(type_name))
+        # save_root = '../DataSets/Anomaly/attack-dec-feature-device/{}'.format(type_name)
+        # file_list = file_name_walk('../DataSets/normal-packet-level-device/{:}'.format(type_name))
+        # save_root = '../DataSets/normal-dec-feature-device/{}'.format(type_name)
+        file_list = file_name_walk('../NewDataSets/normal-packet-level-device/{:}'.format(type_name))
+        save_root = '../NewDataSets/normal-dec-feature-device/{}'.format(type_name)
         if not os.path.exists(save_root):
             os.makedirs(save_root)
 
@@ -83,8 +84,8 @@ def main():
 if __name__ == '__main__':
     a = datetime.now()
     print("start time", a)
-    # main()
-    open_source_data_process()
+    main()
+    # open_source_data_process()
     b = datetime.now()
     print("end time", b)
     durn = (b-a).seconds
