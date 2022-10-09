f43caab1bb3de3fc2a6a1acd6f7f2ef4d09bc0e0
vicTorKd <81010941+vicTorKd@users.noreply.github.com>
2022-10-09 19:43:30 +0800

    Update test_normal_dec.py

--- pcap_process/test_normal_dec.py
+++ pcap_process/test_normal_dec.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2021/7/14 下午3:51
-# @Author  : Yutao Dong
-# @Site    : 
-# @File    : test_normal_dec.py
-# @Software: PyCharm
 import pandas as pd
 import numpy as np
 from datetime import datetime
@@ -16,14 +9,11 @@ normal_type = 10
 def file_name_walk(file_dir):
     file_list = []
     for root, dirs, files in os.walk(file_dir):
-        # print("root", root)  # 当前目录路径
-        # print("dirs", dirs)  # 当前路径下所有子目录
-        # print("files", files)  # 当前路径下所有非目录子文件
         for file in files:
             if os.path.splitext(file)[1] == ".pcap":
                 file_list.append("{}/{}".format(root, file))
     print(file_list)
     return file_list
 
-file_list = file_name_walk('/home/dyt/IForest_IoT/DataSets/Normal/data/aqara_gateway')
+file_list = file_name_walk('../DataSets/Normal/data/aqara_gateway')
 print(file_list[27])
