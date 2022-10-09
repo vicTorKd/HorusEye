e90889bc178570e797fc1f0a96899eac557103e2
vicTorKd <81010941+vicTorKd@users.noreply.github.com>
2022-10-09 19:44:17 +0800

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,9 +1,1 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2021/11/30 下午5:04
-# @Author  : Yutao Dong
-# @Site    : 
-# @File    : __init__.py
-# @Software: PyCharm
-
 from .AE import *
