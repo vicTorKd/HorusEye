244431829e6cf43d5b684bf14493196ab47468eb
VicTor_Wu <81010941+vicTorKd@users.noreply.github.com>
2023-06-02 16:31:46 +0800

    Update AE.py

--- model/AE.py
+++ model/AE.py
@@ -10,54 +10,9 @@ class hswish(nn.Module):
         return out
 
 
-class Kitsune(nn.Module):  # Kitsune model when m=1, used for complexity test
-    def __init__(self, input_size=100, hidden_rate=0.75):
-        super(Kitsune, self).__init__()
-        self.input_size = input_size
-        self.hidden_rate = hidden_rate
-        self.thr = 1
-        self.EnsembleLayer = nn.Sequential(
-            # encoder
-            nn.Conv1d(in_channels=self.input_size, out_channels=self.input_size, kernel_size=1,  # 100
-                      stride=1, bias=False, groups=self.input_size),
-            nn.Sigmoid(),
-            # decoder
-            nn.Conv1d(in_channels=self.input_size, out_channels=self.input_size, kernel_size=1,  # 100
-                      stride=1, bias=False, groups=self.input_size),
-            nn.Sigmoid()
-        )
-        self.OutputLayer = nn.Sequential(
-            nn.Linear(self.input_size, int(self.input_size * self.hidden_rate)),
-            nn.Sigmoid(),
-            nn.Linear(int(self.input_size * self.hidden_rate), self.input_size),
-            nn.Sigmoid()
-        )
-
-    def forward(self, x):
-        # reshape input to 100 channels for conv1d
-        x = torch.reshape(x, (-1, self.input_size, 1))
-        out = self.EnsembleLayer(x)
-        # reshape output to 1 channel for ae
-        out = torch.reshape(out, (-1, 1, self.input_size))
-        out = self.OutputLayer(out)
-        return out
-
-    def excute_RMSE(self, input, target):
-        mse = (input - target).pow(2).sum(2) / self.input_size
-        rmse = torch.sqrt(mse)
-        return rmse.detach().cpu().numpy().reshape(-1)  # 0 :error 1:normal
-
-    def pred(self, input, target):
-        rmse = self.excute_RMSE(input, target)
-        return rmse > self.thr
-
-    def set_thr(self, thr):
-        self.thr = thr
-
-
-class CNN_AE_channel(nn.Module):
+class Magnifier(nn.Module):
     def __init__(self, input_size=16):
-        super(CNN_AE_channel, self).__init__()
+        super(Magnifier, self).__init__()
 
         self.input_size = input_size
         self.thr = 1
